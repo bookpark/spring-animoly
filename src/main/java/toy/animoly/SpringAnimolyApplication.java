a5fcd9931eab0d0b38cdf09077d3145d34b9dd25
bookpark <bkbkgg@gmail.com>
2023-01-18 21:58:48 +0900

    initial commit

---
+++ src/main/java/toy/animoly/SpringAnimolyApplication.java
@@ -0,0 +1,13 @@
+package toy.animoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnimolyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAnimolyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/toy/animoly/SpringAnimolyApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.animoly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnimolyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
