d34247ae95561ecc69c96ef1521f0ca6183dce46
Booki Park <bkbkgg@gmail.com>
2023-01-19 21:15:31 +0900

    @Repository 추가

--- src/main/java/toy/animoly/repository/UserRepository.java
+++ src/main/java/toy/animoly/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package toy.animoly.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import toy.animoly.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 }
