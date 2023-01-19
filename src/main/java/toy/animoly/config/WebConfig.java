feb49d6de383b50641902fb8846dc6f3422cce5a
Booki Park <bkbkgg@gmail.com>
2023-01-19 21:34:16 +0900

    WebConfig 추가

---
+++ src/main/java/toy/animoly/config/WebConfig.java
@@ -0,0 +1,16 @@
+package toy.animoly.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // CORS 설정
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+    }
+}
