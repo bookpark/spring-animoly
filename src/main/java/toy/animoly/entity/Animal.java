35d7a2e666887bd97fbf606fad0bedd473342fa1
Booki Park <bkbkgg@gmail.com>
2023-01-26 01:31:20 +0900

    Animal 엔티티 필드 추가

--- src/main/java/toy/animoly/entity/Animal.java
+++ src/main/java/toy/animoly/entity/Animal.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.awt.print.Book;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +18,29 @@ public class Animal {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "animal_id")
     private Long id;
+    private Long desertionNo;
+    private String filename;
+    private LocalDateTime happenDt;
+    private String happenPlace;
+    private String kindCd;
+    private String colorCd;
+    private Integer age;
+    private Integer weight;
+    private Long noticeNo;
+    private LocalDateTime noticeSdt;
+    private LocalDateTime noticeEdt;
+    private String popfile;
+    private String processState;
+    private String sexCd;
+    private Boolean neuterYn;
+    private String specialMark;
+    private String careNm;
+    private String careTel;
+    private String careAddr;
+    private String orgNm;
+    private String chargeNm;
+    private String officetel;
+    private String noticeComment;
     @OneToMany(mappedBy = "animal")
     private List<Adoption> adoptions = new ArrayList<>();
     @OneToMany(mappedBy = "animal")
