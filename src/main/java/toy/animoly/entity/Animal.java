e2796d6d1d9600c782c2ea5058f5474611165520
Booki Park <bkbkgg@gmail.com>
2023-03-16 21:18:04 +0900

    api 데이터에 맞는 type 변경

--- src/main/java/toy/animoly/entity/Animal.java
+++ src/main/java/toy/animoly/entity/Animal.java
@@ -5,35 +5,34 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.awt.print.Book;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
 public class Animal {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "animal_id")
     private Long id;
     private Long desertionNo;
     private String filename;
-    private LocalDateTime happenDt;
+    private String happenDt;
     private String happenPlace;
     private String kindCd;
     private String colorCd;
-    private Integer age;
-    private Integer weight;
-    private Long noticeNo;
-    private LocalDateTime noticeSdt;
-    private LocalDateTime noticeEdt;
+    private String age;
+    private String weight;
+    private String noticeNo;
+    private Date noticeSdt;
+    private Date noticeEdt;
     private String popfile;
     private String processState;
     private String sexCd;
-    private Boolean neuterYn;
+    private String neuterYn;
     private String specialMark;
     private String careNm;
     private String careTel;
@@ -47,5 +46,4 @@ public class Animal {
     private Adoption adoption;
     @OneToMany(mappedBy = "animal")
     private List<Bookmark> bookmarks = new ArrayList<>();
-
 }
