b3a102ac9ff7c04c8e4f0b24363517988fcf6d5d
Booki Park <bkbkgg@gmail.com>
2023-03-17 21:10:05 +0900

    회원가입 및 중복회원 검증 메서드 테스트 코드 작성

---
+++ src/test/java/toy/animoly/service/UserServiceTest.java
@@ -0,0 +1,49 @@
+package toy.animoly.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import toy.animoly.entity.User;
+import toy.animoly.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void join() {
+        //given
+        User user = new User();
+        user.setId("bookpark");
+
+        //when
+        String savedId = userService.join(user);
+
+        //then
+        assertEquals("bookpark", userRepository.findById(savedId).get().getId());
+
+    }
+
+    @Test
+    void validateDuplicateUser() throws Exception {
+        //given
+        User user1 = new User();
+        user1.setId("bookpark");
+        User user2 = new User();
+        user2.setId("bookpark");
+
+        //when
+        userService.join(user1);
+
+        //then
+        IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> userService.join(user2));
+        assertEquals("이미 존재하는 ID입니다.", thrown.getMessage());
+    }
+}
