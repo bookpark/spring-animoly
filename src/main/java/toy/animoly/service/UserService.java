4454ab3dbb8142a7a333b76804f3d8cf20466fec
Booki Park <bkbkgg@gmail.com>
2023-03-17 21:09:56 +0900

    회원가입 및 중복회원 검증 메서드 추가

---
+++ src/main/java/toy/animoly/service/UserService.java
@@ -0,0 +1,35 @@
+package toy.animoly.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import toy.animoly.entity.User;
+import toy.animoly.repository.UserRepository;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    /**
+     * 회원가입
+     */
+    public String join(User user) {
+        validateDuplicateUser(user);
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    /**
+     * 중복 회원 검사
+     */
+    private void validateDuplicateUser(User user) {
+        Optional<User> findUsers = userRepository.findById(user.getId());
+        if (findUsers.isPresent()) {
+            throw new IllegalStateException("이미 존재하는 ID입니다.");
+        }
+    }
+}
