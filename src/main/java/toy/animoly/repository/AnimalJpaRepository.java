1a6678a3d428bb78f9681abfe0f3a07af742ab93
Booki Park <bkbkgg@gmail.com>
2023-03-21 18:31:17 +0900

    Querydsl 사용을 위한 repository 생성

---
+++ src/main/java/toy/animoly/repository/AnimalJpaRepository.java
@@ -0,0 +1,28 @@
+package toy.animoly.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+import toy.animoly.entity.Animal;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static toy.animoly.entity.QAdoption.*;
+import static toy.animoly.entity.QAnimal.*;
+import static toy.animoly.entity.QBookmark.*;
+
+@Repository
+public class AnimalJpaRepository {
+    private final JPAQueryFactory query;
+
+    public AnimalJpaRepository(EntityManager em) {
+        this.query = new JPAQueryFactory(em);
+    }
+
+    public List<Animal> findAll() {
+        return query
+                .select(animal)
+                .from(animal)
+                .fetch();
+    }
+}
