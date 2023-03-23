c2b7e26e60fe2cb31f7482267b4380b2e9bbc419
Booki Park <bkbkgg@gmail.com>
2023-03-23 20:46:00 +0900

    PasswordEncoder 추가

--- src/main/java/toy/animoly/config/SecurityConfig.java
+++ src/main/java/toy/animoly/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package toy.animoly.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -13,6 +15,10 @@ import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // REST API를 사용하며 쿠키를 사용하는 방식으로 인증을 하지 않기 때문에 CSRF disable 처리함
