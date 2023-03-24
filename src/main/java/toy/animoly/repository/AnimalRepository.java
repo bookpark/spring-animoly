881986b0118fbb3a83de8dba8b1a2dc84ebfd4e7
Booki Park <bkbkgg@gmail.com>
2023-03-24 18:18:57 +0900

    findByDesertionNo() 메서드 추가

--- src/main/java/toy/animoly/repository/AnimalRepository.java
+++ src/main/java/toy/animoly/repository/AnimalRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import toy.animoly.entity.Animal;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+    Animal findByDesertionNo(Long desertionNo);
 }
