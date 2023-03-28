28b080d2e031d8728e258d991e70139c8fe08f08
Booki Park <bkbkgg@gmail.com>
2023-03-28 21:12:03 +0900

    Blog 작성 기능 테스트 코드 추가

---
+++ src/test/java/toy/animoly/service/BlogServiceTest.java
@@ -0,0 +1,46 @@
+package toy.animoly.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import toy.animoly.entity.Blog;
+import toy.animoly.entity.User;
+import toy.animoly.repository.BlogRepository;
+import toy.animoly.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class BlogServiceTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BlogRepository blogRepository;
+    @Autowired
+    private BlogService blogService;
+
+    @Test
+    void create() {
+        //given
+        User user = new User();
+        user.setId("bookpark");
+        userRepository.save(user);
+        String title = "제목";
+        String content = "내용";
+
+        //when
+        Long findBlog = blogService.create(user.getId(), title, content);
+        Blog blog = blogRepository.findById(findBlog).orElseThrow();
+
+        //then
+        assertEquals("제목", blog.getTitle());
+        assertEquals("내용", blog.getContent());
+    }
+
+    @Test
+    void update() {
+    }
+}
