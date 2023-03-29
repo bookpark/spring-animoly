29d01dcba71c05334fe23a0210cb17dc711d02f2
Booki Park <bkbkgg@gmail.com>
2023-03-29 17:52:17 +0900

    @Repository 애노태이션 삭제

--- src/main/java/toy/animoly/repository/UserRepository.java
+++ src/main/java/toy/animoly/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package toy.animoly.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import toy.animoly.entity.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 }
