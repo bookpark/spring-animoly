ae720f4ab9985fad5825504f61389192bfb17c24
Booki Park <bkbkgg@gmail.com>
2023-04-08 18:32:20 +0900

    입양 취소 요청 API 추가

--- src/main/java/toy/animoly/controller/AdoptionApiController.java
+++ src/main/java/toy/animoly/controller/AdoptionApiController.java
@@ -3,16 +3,28 @@ package toy.animoly.controller;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import toy.animoly.entity.Adoption;
+import toy.animoly.entity.AdoptionStatus;
 import toy.animoly.service.AdoptionService;
+
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class AdoptionApiController {
     private final AdoptionService adoptionService;
 
-    @PostMapping("/api/apply")
-    public Long apply(@RequestBody CreateAdoptionRequest request) {
-        return adoptionService.apply(request.userId, request.animalId);
+    @PostMapping("/api/adoptions/apply")
+    public AdoptionStatus apply(CreateAdoptionRequest request) {
+        Long id = adoptionService.apply(request.userId, request.animalId);
+        return adoptionService.findById(id).orElseThrow().getStatus();
+    }
+
+    @PostMapping("/api/adoptions/{id}/request-cancel")
+    public AdoptionStatus requestCancel(@PathVariable Long id) {
+        Optional<Adoption> adoption = adoptionService.findById(id);
+        adoptionService.requestCancel(id);
+        return adoption.orElseThrow().getStatus();
     }
 
     @Data
