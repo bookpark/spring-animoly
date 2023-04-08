1e9c213e840f388a5c64345ff38e42002668896b
Booki Park <bkbkgg@gmail.com>
2023-04-08 18:43:39 +0900

    입양 취소 요청 API 테스트 코드 작성

--- src/test/java/toy/animoly/controller/AdoptionApiControllerTest.java
+++ src/test/java/toy/animoly/controller/AdoptionApiControllerTest.java
@@ -6,16 +6,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.transaction.annotation.Transactional;
 import toy.animoly.entity.Animal;
 import toy.animoly.entity.User;
 import toy.animoly.repository.AnimalRepository;
 import toy.animoly.repository.UserRepository;
+import toy.animoly.service.AdoptionService;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static toy.animoly.entity.AdoptionStatus.CANCEL_REQUESTED;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,6 +26,8 @@ class AdoptionApiControllerTest {
     private UserRepository userRepository;
     @Autowired
     private AnimalRepository animalRepository;
+    @Autowired
+    private AdoptionService adoptionService;
 
     @Test
     void apply() throws Exception {
@@ -36,10 +38,31 @@ class AdoptionApiControllerTest {
         animal.setDesertionNo(1L);
         animalRepository.save(animal);
         String requestJson = "{\"userId\":\"bookpark\", \"animalId\": \"1\"}";
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/apply")
+        mockMvc.perform(post("/api/apply")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void requestCancel() throws Exception {
+        //given
+        User user = new User();
+        user.setId("bookpark");
+        userRepository.save(user);
+        Animal animal = new Animal();
+        animal.setDesertionNo(1L);
+        animalRepository.save(animal);
+        Long id = adoptionService.apply(user.getId(), animal.getDesertionNo());
+
+        //when
+        adoptionService.requestCancel(id);
+
+        //then
+        mockMvc.perform(post("/api/adoptions/1/request-cancel"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(String.valueOf("\"CANCEL_REQUESTED\"")));
     }
 }
